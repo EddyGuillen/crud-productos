aa28b3255b9422f8c15bf2722bccad75f996f375
EddyGuillen <eddy10bryan@gmail.com>
2023-02-20 20:55:15 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 const d = document,
-  $table = d.querySelector(".crud-table"),
-  $template = d.getElementById("crud-template").content,
+  $table = d.querySelector('.crud-table'),
+  $template = d.getElementById('crud-template').content,
   $fragment = d.createDocumentFragment(),
-  $select = d.querySelector(".form-select"),
-  $btnSearch = d.getElementById("btn-search"),
-  $inputSearch = d.getElementById("input-search"),
-  $form = d.querySelector(".crud-form"),
-  $addProduct = d.getElementById("add-product"),
-  $formAdd = d.getElementById("form-add"),
-  $formEdit = d.getElementById("form-edit");
+  $select = d.querySelector('.form-select'),
+  $btnSearch = d.getElementById('btn-search'),
+  $inputSearch = d.getElementById('input-search'),
+  $form = d.querySelector('.crud-form'),
+  $addProduct = d.getElementById('add-product'),
+  $formAdd = d.getElementById('form-add'),
+  $formEdit = d.getElementById('form-edit');
 
 const getAll = async () => {
   try {
@@ -27,44 +27,47 @@ const getAll = async () => {
     });
 
     $select.innerHTML = $options;
-    d.getElementById("select-modal").innerHTML = $options;
-    d.getElementById("select-modal-edit").innerHTML = $options;
+    d.getElementById('select-modal').innerHTML = $options;
+    d.getElementById('select-modal-edit').innerHTML = $options;
+    let index = 0;
 
     productsData.forEach((el) => {
-      $template.querySelector(".name").textContent = el.name;
-      $template.querySelector(".category").textContent = el.category;
-      $template.querySelector(".date").textContent = el.registrationDate;
-      $template.querySelector(".stock").textContent = el.stock;
-      $template.querySelector(".edit").dataset.id = el.name;
-      $template.querySelector(".edit").dataset.name = el.name;
-      $template.querySelector(".edit").dataset.category = el.category;
-      $template.querySelector(".edit").dataset.stock = el.stock;
-      $template.querySelector(".edit").dataset.registrationDate =
+      index++;
+      $template.querySelector('.index').textContent = index;
+      $template.querySelector('.name').textContent = el.name;
+      $template.querySelector('.category').textContent = el.category;
+      $template.querySelector('.date').textContent = el.registrationDate;
+      $template.querySelector('.stock').textContent = el.stock;
+      $template.querySelector('.edit').dataset.id = el.id;
+      $template.querySelector('.edit').dataset.name = el.name;
+      $template.querySelector('.edit').dataset.category = el.category;
+      $template.querySelector('.edit').dataset.stock = el.stock;
+      $template.querySelector('.edit').dataset.registrationDate =
         el.registrationDate;
-      $template.querySelector(".delete").dataset.id = el.id;
+      $template.querySelector('.delete').dataset.id = el.id;
 
       let $clone = d.importNode($template, true);
       $fragment.appendChild($clone);
     });
 
-    $table.querySelector("tbody").appendChild($fragment);
+    $table.querySelector('tbody').appendChild($fragment);
   } catch (err) {
-    let message = err.statusText || "Ocurrió un error";
+    let message = err.statusText || 'Ocurrió un error';
     $table.insertAdjacentElement(
-      "afterend",
+      'afterend',
       `<p><b>Error ${err.status}: ${message}</b></p>`
     );
   }
 };
 
-d.addEventListener("DOMContentLoaded", getAll);
+d.addEventListener('DOMContentLoaded', getAll);
 
-d.addEventListener("submit", async (e) => {
+d.addEventListener('submit', async (e) => {
   /* <----------- Search ---------->*/
   if (e.target === $form) {
     e.preventDefault();
 
-    console.log("prueba");
+    console.log('prueba');
     console.log($select.value);
 
     try {
@@ -76,50 +79,51 @@ d.addEventListener("submit", async (e) => {
       if (!res.ok) throw { status: res.status, statusText: res.statusText };
 
       if (json.length > 0) {
-        d.querySelector(".alert").textContent = "";
+        d.querySelector('.alert').textContent = '';
         json.forEach((el) => {
-          $template.querySelector(".name").textContent = el.name;
-          $template.querySelector(".category").textContent = el.category;
-          $template.querySelector(".date").textContent = el.registrationDate;
-          $template.querySelector(".stock").textContent = el.stock;
-          $template.querySelector(".edit").dataset.id = el.name;
-          $template.querySelector(".edit").dataset.name = el.name;
-          $template.querySelector(".edit").dataset.category = el.category;
-          $template.querySelector(".edit").dataset.stock = el.stock;
-          $template.querySelector(".edit").dataset.registrationDate =
+          $template.querySelector('.name').textContent = el.name;
+          $template.querySelector('.category').textContent = el.category;
+          $template.querySelector('.date').textContent = el.registrationDate;
+          $template.querySelector('.stock').textContent = el.stock;
+          $template.querySelector('.edit').dataset.id = el.name;
+          $template.querySelector('.edit').dataset.name = el.name;
+          $template.querySelector('.edit').dataset.category = el.category;
+          $template.querySelector('.edit').dataset.stock = el.stock;
+          $template.querySelector('.edit').dataset.registrationDate =
             el.registrationDate;
-          $template.querySelector(".delete").dataset.id = el.id;
+          $template.querySelector('.delete').dataset.id = el.id;
 
           let $clone = d.importNode($template, true);
           $fragment.appendChild($clone);
         });
-        $table.querySelector("tbody").innerHTML = "";
-        $table.querySelector("tbody").appendChild($fragment);
+        $table.querySelector('tbody').innerHTML = '';
+        $table.querySelector('tbody').appendChild($fragment);
       } else {
-        $table.querySelector("tbody").innerHTML = "";
-        d.querySelector(".alert").textContent = "No se encontraron resultados";
+        $table.querySelector('tbody').innerHTML = '';
+        d.querySelector('.alert').textContent = 'No se encontraron resultados';
       }
     } catch (err) {
-      let message = err.statusText || "Ocurrió un error";
+      let message = err.statusText || 'Ocurrió un error';
       console.log(err);
       $table.insertAdjacentHTML(
-        "afterend",
+        'afterend',
         `<p><b>Error ${err.status}: ${message}</b></p>`
       );
     }
   }
 
+  let date = new Date(Date.now());
+  date = date.toLocaleDateString('en-GB');
   /* CREATE-POST */
+
   if (e.target === $formAdd) {
-    let date = new Date(Date.now());
-    date = date.toLocaleDateString("en-GB");
     e.preventDefault();
 
     try {
       let options = {
-          method: "POST",
+          method: 'POST',
           headers: {
-            "Content-type": "application/json; charset=utf-8",
+            'Content-type': 'application/json; charset=utf-8',
           },
           body: JSON.stringify({
             name: e.target.name.value,
@@ -128,22 +132,85 @@ d.addEventListener("submit", async (e) => {
             registrationDate: date,
           }),
         },
-        res = await fetch("http://localhost:3000/products", options),
+        res = await fetch('http://localhost:3000/products', options),
         json = await res.json();
       if (!res.ok) throw { status: res.status, statusText: res.statusText };
     } catch (err) {
-      let message = err.statusText || "Ocurrió un error";
+      let message = err.statusText || 'Ocurrió un error';
       $formAdd.insertAdjacentHTML(
-        "afterend",
+        'afterend',
         `<p><b>Error ${err.status}: ${message}</b></p>`
       );
     }
   }
 
-  /* UPDATE- PUT */
+  //UPDATE- PUT
   if (e.target === $formEdit) {
+    //UPDATE - PUT
     try {
-      let options;
-    } catch (err) {}
+      let options = {
+          method: 'PUT',
+          headers: {
+            'Content-type': 'application/json; charset=utf-8',
+          },
+          body: JSON.stringify({
+            name: e.target.name.value,
+            category: e.target.category.value,
+            stock: e.target.stock.value,
+            registrationDate: e.target.date.value,
+          }),
+        },
+        res = await fetch(
+          `http://localhost:3000/products/${e.target.id.value}`,
+          options
+        ),
+        json = await res.json();
+      if (!res.ok) throw { status: res.status, statusText: res.statusText };
+      location.reload();
+    } catch (err) {
+      let message = err.statusText || 'Ocurrió un error';
+      $form.insertAdjacentHTML(
+        'afterend',
+        `<p><b>Error ${err.status}: ${message}</b></p>`
+      );
+    }
+  }
+});
+
+d.addEventListener('click', async (e) => {
+  if (e.target.matches('.edit')) {
+    $formEdit.name.value = e.target.dataset.name;
+    $formEdit.category.value = e.target.dataset.category;
+    $formEdit.stock.value = e.target.dataset.stock;
+    $formEdit.id.value = e.target.dataset.id;
+    $formEdit.date.value = e.target.dataset.registrationDate;
+  }
+
+  if (e.target.matches('.delete')) {
+    let isDelete = confirm(
+      `¿Esta seguro de eliminar el id ${e.target.dataset.id}?`
+    );
+
+    if (isDelete) {
+      //DELETE -DELETE
+      try {
+        let options = {
+            method: 'DELETE',
+            headers: {
+              'Content-type': 'application/json; charset=utf-8',
+            },
+          },
+          res = await fetch(
+            `http://localhost:3000/products/${e.target.dataset.id}`,
+            options
+          ),
+          json = await res.json();
+        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+        location.reload();
+      } catch (err) {
+        let message = err.statusText || 'Ocurrió un error';
+        alert(`Error ${err.status}: ${message}`);
+      }
+    }
   }
 });
